7c0062e57bcaa513e8042f3ad747e537bafd5d8e
KostadinTashev <kostadin.tashev2002@gmail.com>
2024-03-13 10:19:21 +0200

    Forms

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, resolve_url
 import pyperclip
 from django.urls import reverse
 
+from petstagram.common.forms import PhotoCommentForm, SearchPhotosForm
 from petstagram.common.models import PhotoLike
 from petstagram.common.utils import get_user_liked_photos, get_photo_url
 from petstagram.core.photo_utils import apply_likes_count, apply_user_liked_photo
@@ -9,12 +10,24 @@ from petstagram.photos.models import Photo
 
 
 def index(request):
-    photos = [apply_likes_count(photo) for photo in Photo.objects.all()]
+    search_form = SearchPhotosForm(request.GET)
+    search_pattern = None
+    if search_form.is_valid():
+        search_pattern = search_form.cleaned_data['pet_name']
+
+    photos = Photo.objects.all()
+    if search_pattern:
+        photos = photos.filter(tagged_pets__name__icontains=search_pattern)
+
+    photos = [apply_likes_count(photo) for photo in photos]
     photos = [apply_user_liked_photo(photo) for photo in photos]
 
     context = {
         'photos': photos,
+        'comment_form': PhotoCommentForm(),
+        'search_form': search_form,
     }
+
     return render(
         request,
         'common/home-page.html',
@@ -48,3 +61,19 @@ def share_photo(request, photo_id):
 
     pyperclip.copy(photo_details_url)
     return redirect(get_photo_url(request, photo_id))
+
+
+def comment_photo(request, photo_id):
+    photo = Photo.objects.filter(pk=photo_id).get()
+
+    if request.method == 'POST':
+        form = PhotoCommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.photo = photo
+            comment.save()
+            return redirect('index')
+    else:
+        form = PhotoCommentForm()
+
+    return redirect('index')
